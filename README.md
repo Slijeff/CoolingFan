ef091b03df3404b681fc2efbe1882fca2473fb6f
Slijeff <jeff83636tx@gmail.com>
2021-05-26 19:12:24 +0800

    changed README and changed background color

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # A cooling fan the helps you cool down
 It's soooooo hot!
+
+# Todo
+* Add dark mode support
+* Add Chinese
+* Add sound effects
+* Add more modes
--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import FastButton from './Components/buttonFast'
 function App() {
   
   return (
-    <div App className="flex justify-center min-h-screen items-center flex-col space-x-3 space-y-3">
+    <div App className="flex justify-center min-h-screen items-center flex-col space-x-3 space-y-3 bg-gray-100">
       <p className="font-mono text-4xl">Cooling Fan</p>
-      <p className="font-mono text-gray-400">by Jeffrey Hui</p>
+      <p className="font-mono text-gray-400">author: Jeffrey Hui</p>
       <Fan></Fan>
       <div id="buttons" className="flex justify-between items-center flex-row space-x-3">
         <StopButton/>
@@ -17,6 +17,7 @@ function App() {
         <MediumButton></MediumButton>
         <FastButton></FastButton>
       </div>
+      <p className="font-mono text-gray-300 mt-10">Source: <a href="https://github.com/Slijeff/CoolingFan"> https://github.com/Slijeff/CoolingFan</a></p>
       
     </div>
   );
